737fadd1e9570ad17fb0b551629e3fbed141e259
Arthur Carson <rthrcrsn@gmail.com>
2020-09-16 15:31:53 -0500

    new user stuff

--- src/App.js
+++ src/App.js
@@ -7,24 +7,68 @@ class App extends Component {
   state = {
     cohortName: 'Tarjan',
     salutation: 'Swing Swing',
-    name: 'Arthur',
-    whereILive: 'Nokomis East',
-    pizzaTopping: 'green olives'
+    user: {
+      name: 'Arthur',
+      whereILive: 'Uptown',
+      pizzaTopping: 'roasted red peppers'
+    }
+  }
+
+  // Mutates the state and user object
+  // this.state.user.pizzaTopping = newPizzaTopping;
+
+  onChangePizza = (event) => {
+    let newPizzaTopping = event.target.value;
+    console.log(`changed pizza topping to: `, newPizzaTopping);
+
+    /*
+    let oldUser = this.state.user;
+    let newUser = {
+      ...oldUser,
+      pizzaTopping: newPizzaTopping
+    }
+    
+    this.setState({
+      user: newUser
+    });
+    */
+   
+    this.setState({
+      // Create a new user object for our state
+     // aka "immutable" state
+     // like a new carbon copy 
+      user: {
+        // include data from existing user object
+        // name: this.state.user.name,
+        // whereILive: this.state.user.whereILive,
+        // modify new state
+        // pizzaTopping: newPizzaTopping
+
+        // New syntax
+        // creates a new object
+        ...this.state.user,
+        pizzaTopping: newPizzaTopping
+      }
+    })
   }
 
   render() {
+    console.log('state', this.state)
     return (
       <div>
       <h1>{this.state.cohortName} State Learning</h1>
 
           <div>
-          {this.state.salutation}! My name is {this.state.name}
+            {this.state.salutation}! My name is {this.state.user.name}
           </div>
           <ul>
-            <li>My name is {this.state.name}.</li>
-            <li>I live in {this.state.whereILive}</li>
-            <li>My favorite pizza topping is {this.state.pizzaTopping}</li>
+            <li>My name is {this.state.user.name}.</li>
+            <li>I live in {this.state.user.whereILive}.</li>
+            <li>My favorite pizza topping is {this.state.user.pizzaTopping}.</li>
           </ul>
+
+          <h4>Edit user form</h4>
+          <input type="text" placeholder="Favorite Pizza Topping" onChange={this.onChangePizza}/>
       </div>
     )
   }
